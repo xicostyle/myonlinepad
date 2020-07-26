029ca3bc1b10d741814598b16442b8d2214b9e96
Lucas Heitor <lh.maschio.maia@gmail.com>
2020-07-25 21:55:08 -0300

    Inicializando front

--- myonlinepad/src/app/app.module.ts
+++ myonlinepad/src/app/app.module.ts
@@ -1,22 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule, MatError } from '@angular/material/form-field';
+import { MatInputModule  } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MyPadOnlineCadastrarSeComponent } from './components/my-pad-online-cadastrar-se/my-pad-online-cadastrar-se.component';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MyPadOnlineCadastrarSeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  exports: [
+    MyPadOnlineCadastrarSeComponent
+  ],
+  providers: [
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- myonlinepad/src/app/home/home.component.ts
+++ myonlinepad/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MyPadOnlineCadastrarSeComponent } from '../components/my-pad-online-cadastrar-se/my-pad-online-cadastrar-se.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  cadastrarSe() {
+
+      const dialogRef = this.dialog.open(MyPadOnlineCadastrarSeComponent, {
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+      });
+
+  }
+
+
+  entrar() {
+
+  }
 }
